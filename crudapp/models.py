4ef2d5f2cfbb4f2a0b1ddda064f690fcf29880bf
dbfla8772 <dbfla5527@naver.com>
2021-08-31 19:39:28 +0900

    add files for deploying to heroku

--- crudapp/models.py
+++ crudapp/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=200)
-    writer = models.CharField(max_length=100)
+    title = models.CharField('제목', max_length=200)
+    writer = models.CharField('작가', max_length=100)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True)
     pub_date = models.DateTimeField('date published')
-    body = models.TextField()
-    images = models.ImageField(blank=True, upload_to="images", null=True)
+    body = models.TextField('내용')
+    images = models.ImageField('표지', blank=True, upload_to="images", null=True)
     image_thumbnail = ImageSpecField(source = 'images', processors=[ResizeToFill(120,60)], format='JPEG')
     hits = models.PositiveBigIntegerField(default=0, verbose_name='조회수')
 
